8a33c6b1deec6ab6fbf2983f7b1a62c6324a8a4f
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-04-19 16:16:12 +0300

    Views(Tag List,Tag Details) for store app

--- store/onlinestore/views.py
+++ store/onlinestore/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import Category,Product,Tag
 from django.views.generic import View
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 
 class CategoryList(View):
@@ -25,6 +26,30 @@ class CategoryDetails(View):
         category=get_object_or_404(self.model,slug__iexact=slug)
         context={'category':category}
         return render(request,self.template_name,context=context)
+
+
+class TagList(View):
+    '''Class to display a list of all Tag Objects'''
+
+    model=Tag
+    template_name='onlinestore/tag_list.html'
+
+    def get(self,request):
+        tag_list=self.model.objects.all()
+        context={'tag_list':tag_list}
+        return render(request,self.template_name,context=context)
+
+class TagDetails(View):
+    '''Class to display all the details for a Tag object'''
+
+    model=Tag
+    template_name='onlinestore/tag_details.html'
+
+    def get(self,request,slug):
+        tag=get_object_or_404(self.model,slug__iexact=slug)
+        context={'tag':tag}
+        return render(request,self.template_name,context=context)
+
 
 
 
