c20a0a9bd9bf4e55e0ba6a96a3cfca248630ac2d
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-04-20 16:27:57 +0300

    Contact app (View to display contact form)

--- store/contact/views.py
+++ store/contact/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.messages import success
+from django.views.generic import View
+from .forms import ContactForm
 
-# Create your views here.
+
+class ContactView(View):
+    '''Class to construct a view to display a contact form for the users'''
+
+    form_class=ContactForm
+    template_name='contact/contact_form.html'
+
+    def get(self,request):
+        return render(request,self.template_name,{'form':self.form_class()})
+
+    def post(self,request):
+
+        bound_form=self.form_class(request.POST)
+        if bound_form.is_valid():
+            mail_sent=bound_form.send_email()
+
+            if mail_sent:
+                success(request,'Email successfully sent')
+                return redirect('product_list')
+        else:
+            return render(request,self.template_name,{'form':bound_form})
+
+
+
+
+
