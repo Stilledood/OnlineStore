12c82c15812f59c0c4222cc08bdb052a6dff13a0
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-04-21 19:40:50 +0300

    Blog app urlpatterns updated

--- store/blog/urls.py
+++ store/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import re_path
-from .views import PostList,PostDetails
+from .views import PostList,PostDetails,PostAdd,PostUpdate,PostDelete
 
 urlpatterns=[
     re_path(r'^$',PostList.as_view(),name='post_list'),
     re_path(r'^(?P<pk>\d+)/$',PostDetails.as_view(),name='post_details'),
+    re_path(r'^add/$',PostAdd.as_view(),name='post_add'),
+    re_path(r'^(?P<pk>\d+)/chnage/$',PostUpdate.as_view(),name='post_change'),
+    re_path(r'^(?P<pk>\d+)/delete/$',PostDelete.as_view(),name='post_delete'),
 ]
