6fa5609aececd84a07bdc0be399e35317b41ac58
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-04-21 19:58:27 +0300

    Onlinestore Urlpatterns updated

--- store/onlinestore/urls/product.py
+++ store/onlinestore/urls/product.py
@@ -1,7 +1,10 @@
 from django.urls import re_path
-from ..views import ProductList,ProductDetails
+from ..views import ProductList,ProductDetails,ProductAdd,ProductUpdate,ProductDelete
 
 urlpatterns=[
     re_path(r'^$',ProductList.as_view(),name='product_list'),
     re_path(r'^(?P<pk>\d+)/$',ProductDetails.as_view(),name='product_details'),
+    re_path(r'^add/$',ProductAdd.as_view(),name='product_add'),
+    re_path(r'^(?P<pk>\d+)/change/$',ProductUpdate.as_view(),name='product_update'),
+    re_path(r'^(?P<pk>\d+)/delete/$',ProductDelete.as_view(),name='product_delete'),
 ]
--- store/onlinestore/urls/tag.py
+++ store/onlinestore/urls/tag.py
@@ -1,7 +1,10 @@
 from django.urls import re_path
-from ..views import TagList,TagDetails
+from ..views import TagList,TagDetails,TagAdd,TagUpdate,TagDelete
 
 urlpatterns=[
     re_path(r'^$',TagList.as_view(),name='tag_list'),
     re_path(r'^(?P<slug>[\w\-]+)/$',TagDetails.as_view(),name='tag_details'),
+    re_path(r'^add/$',TagAdd.as_view(),name='tag_add'),
+    re_path(r'^(?P<slug>[\w\-]+)/change/$',TagUpdate.as_view(),name='tag_update'),
+    re_path(r'^(?P<slug>[\w\-]+)/delete/$',TagDelete.as_view(),name='tag_delete'),
 ]
