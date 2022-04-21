f04880b43e41656ea8ef7d67d29140decbb521e7
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-04-21 16:57:14 +0300

    Blog App models

--- store/blog/models.py
+++ store/blog/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.conf import settings
+from onlinestore.models import Tag,Product
 
-# Create your models here.
+
+class Post(models.Model):
+    '''Class to construct a model for Post objects'''
+
+    title=models.CharField(max_length=256)
+    text=models.TextField()
+    date_added=models.DateField(auto_now_add=True)
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='blog_images',default=None)
+    tags=models.ManyToManyField(Tag)
+    products=models.ManyToManyField(Product)
+
+    class Meta:
+        ordering=['-date_added','title']
+        get_latest_by='title'
+
+    def __str__(self):
+        return self.title[:50]
+
+    def get_absolute_url(self):
+        return reverse('post_details',kwargs={'pk':self.pk})
+
+    def get_update_url(self):
+        return reverse('post_update',kwargs={'pk':self.pk})
+
+    def get_delete_url(self):
+        return reverse('post_delete',kwargs={'pk':self.pk})
+
+
+
+class Commnent(models.Model):
+    '''Class to create a model for commnet object'''
+
+    text=models.TextField()
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    date_added=models.DateField(auto_now_add=True)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+
+    class Meta:
+        ordering=['-date_added']
+        get_latest_by='date_added'
+
+    def __str__(self):
+        return self.text[:50]
+
+    def get_delete_url(self):
+        return reverse('commnet_delete',kwargs={'pk':self.pk})
+    
+
+
