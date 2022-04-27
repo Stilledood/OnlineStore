ea6d664d82605987c57e37e6b8a9c20243c71e0a
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-04-27 18:07:40 +0300

    Blog app custom admin models

--- store/blog/admin.py
+++ store/blog/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Post,Commnent
+from django.db.models import Count
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    '''Class to create a custom admin post'''
+
+    list_display=('title','date_added','author','tag_count')
+    date_hierarchy = 'date_added'
+    list_filter=('date_added',)
+    search_fields = ('title','author',)
+
+    fieldsets = (
+        ('Principal',{
+        'fields':('title','text','image')
+        }),
+    ('Related',{
+    'fields':('tags','products','author')
+    }),
+
+    )
+    filter_horizontal = ('tags','products',)
+
+    def get_queryset(self, request):
+        queryset=super().get_queryset(request)
+        return queryset.annotate(tag_number=Count('tags'))
+
+    def tag_count(self,post):
+        return post.tags.count()
+    tag_count.short_description='Number of tags'
+
+
+
+
+
