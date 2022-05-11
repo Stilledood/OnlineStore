ebce3cc0741f73ec18356155cc74d0c8f2309732
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-05-11 14:17:38 +0300

    User Profile Model

--- store/user/models.py
+++ store/user/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.conf import settings
+from django.contrib.auth.models import User,AbstractUser,PermissionsMixin,BaseUserManager
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    '''Class to construct a model for user profile'''
+
+
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='user_images',null=True)
+    name=models.CharField(max_length=128)
+    username=models.CharField(max_length=128)
+
+
+    def __str__(self):
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse('dj-auth:profile_details',kwargs={'username':self.username})
+
+    def get_update_url(self):
+        return reverse('dj-auth:profile_change',kwargs={'username':self.username})
+
+
+@receiver(post_save,sender=User)
+def update_user_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+
+
+
+
+
