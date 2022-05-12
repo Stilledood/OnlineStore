c14b692ec0d44d6b50d4b461dd1e85210b2a426e
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-05-12 17:46:38 +0300

    Shopping cart Models

--- store/shoppingcart/models.py
+++ store/shoppingcart/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.shortcuts import render,redirect
+from onlinestore.models import Product
 
-# Create your models here.
+
+class CartItem(models.Model):
+    '''Class to create a model for each item in shopping cart'''
+
+    cart_id=models.CharField(max_length=50)
+    date_added=models.DateTimeField(auto_now_add=True)
+    quantity=models.IntegerField(default=1)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE,unique=False)
+
+    class Meta:
+        ordering=['date_added']
+
+    def total(self):
+        return self.product.price * self.quantity
+
+    def name(self):
+        return self.product.name
+
+    def price(self):
+        return self.product.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
+
+    def augment_quantity(self,quantity):
+        self.quantity=self.quantity+int(quantity)
+        self.save()
