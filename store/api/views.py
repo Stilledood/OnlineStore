582413c47569048d248785832dba400ae883a8e7
Florin <74854275+Stilledood@users.noreply.github.com>
2022-09-05 12:56:36 +0300

    Added filtering options(ordering,searching,filtering).Also added a custom class to create a custom filter for products

--- store/api/views.py
+++ store/api/views.py
@@ -3,6 +3,8 @@ from .serializers import CategorySerializer,ProductSerializer,PostSerializer
 from rest_framework.generics import ListAPIView,RetrieveAPIView
 from onlinestore.models import Category,Product
 from blog.models import Post
+from rest_framework import filters
+from django_filters import rest_framework as filters
 
 
 class CategoryList(ListAPIView):
@@ -19,11 +21,30 @@ class CategoryDetails(RetrieveAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+class CustomProductFilter(filters.FilterSet):
+    '''Class to construcyt a personalized filter to sort product by date added '''
+
+    from_date_added = filters.DateFilter(field_name='date_added',lookup_expr='gte')
+    until_date_added = filters.DateFilter(field_name='date_added',lookup_expr='lte')
+
+
+    class Meta:
+        model = Product
+        fields=(
+            'date_added',
+            'from_date_added',
+            'until_date_added'
+
+        )
+
+
+
 
 class ProductList(ListAPIView):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_class=CustomProductFilter
     search_fields = ('^name',)
     filter_fields = ('name','date_added')
     ordering_fields = ('name','date_added')
